45d72310f0871bf373481435691f0b674aa1e403
JoseRodolfoHernandez <jrodolfohernandez@ucundinamarca.edu.co>
2026-03-25 00:49:31 -0500

    Implement ML use cases, supervised learning module, and linear regression sales prediction in Flask app

--- LinearRegression.py
+++ LinearRegression.py
@@ -4,6 +4,17 @@ import io
 import base64
 from sklearn.linear_model import LinearRegression
 
+import matplotlib.pyplot as plt
+
+def show_graph(X, Y, model):
+    plt.scatter(X, Y)
+    plt.plot(X, model.predict(X), color='red')
+    plt.title("Linear Regression")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.show()
+
+
 data = {
     "Study Hours": [10, 15, 12, 8, 14, 5, 16, 7, 11, 13, 9, 4, 18, 3, 17, 6, 14, 2, 20, 1],
     "Final Grade": [3.8, 4.2, 3.6, 3, 4.5, 2.5, 4.8, 2.8, 3.7, 4, 3.2, 2.2, 5, 1.8, 4.9, 2.7, 4.4, 1.5, 5, 1]
